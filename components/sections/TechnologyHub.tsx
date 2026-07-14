2e6ae4f52cfdd964f65ba64f6bae3035b85474ca
sbandi22 <sbandi22@asu.edu>
2026-07-14 14:03:41 -0400

    Remove Platform Overview (TechnologySection) from Technology hub

--- components/sections/TechnologyHub.tsx
+++ components/sections/TechnologyHub.tsx
@@ -1,10 +1,8 @@
-import TechnologySection from '@/components/sections/TechnologySection'
 import ArchitectureSection from '@/components/sections/ArchitectureSection'
 
 export default function TechnologyHub() {
   return (
     <div id="technology">
-      <TechnologySection />
       <ArchitectureSection />
     </div>
   )
